b3afec7e3602f80adec71bbf334eeaa807f3d772
QuocNguyen99 <huynhquocnguyen99@gmail.com>
2021-02-01 03:03:10 +0700

    add packgae

--- src/App.js
+++ src/App.js
@@ -3,17 +3,7 @@ import { StyleSheet, Text, View } from 'react-native'
 import useTheme from './Theme/hooks/useTheme'
 
 export default function App() {
-  const {
-    Common,
-    Fonts,
-    Gutters,
-    Images,
-    Layout,
-    Colors,
-    NavigationColors,
-    FontSize,
-    MetricsSizes,
-  } = useTheme();
+
   return (
     <View>
       <Text style={Fonts.textRegular}>Hello</Text>
