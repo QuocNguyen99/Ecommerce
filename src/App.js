852036127ae75446dc47be1500891259ddae06a0
QuocNguyen99 <huynhquocnguyen99@gmail.com>
2021-02-01 01:14:53 +0700

    config basic 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import useTheme from './Theme/hooks/useTheme'
 
 export default function App() {
+  const {
+    Common,
+    Fonts,
+    Gutters,
+    Images,
+    Layout,
+    Colors,
+    NavigationColors,
+    FontSize,
+    MetricsSizes,
+  } = useTheme();
   return (
     <View>
-      <Text>Hello</Text>
+      <Text style={Fonts.textRegular}>Hello</Text>
     </View>
   )
 }
